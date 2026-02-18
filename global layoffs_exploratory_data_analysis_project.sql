961150f471607f69a6043c2f2386f47b9b2a531b
Abraham Bassey <basseyabraham988@gmail.com>
2026-02-18 16:13:30 +0100

    Enhance SQL script with exploratory analysis insights
    Added exploratory analysis comments and insights for layoffs data.

--- global layoffs_exploratory_data_analysis_project.sql
+++ global layoffs_exploratory_data_analysis_project.sql
@@ -5,6 +5,8 @@ FROM layoffs;
 -- 2. standardize the data
 -- 3. Null values or blank values
 -- 4. Remove any columns
+-- 5. Exploratory Analysis
+
 
 -- 1. Remove Duplicates
 
@@ -181,7 +183,7 @@ SELECT *
 FROM layoffs_staging4
 WHERE row_num2 > 1;
                  
--- NULL AND BLANK VALUES
+-- Removing null and blank values
 
 SELECT *
 FROM layoffs_staging4
@@ -273,7 +275,7 @@ SELECT YEAR(`date`), SUM(total_laid_off)
 FROM layoffs_staging4
 GROUP BY YEAR(`date`)
 ORDER BY 1 DESC;	
--- insights
+-- insight;
 -- layoffs peaked in 2023 with 125,677 total layoffs accross countries and industries with just 3 months of data compared to previous years
 
 
@@ -281,7 +283,7 @@ SELECT stage, SUM(total_laid_off)
 FROM layoffs_staging4
 GROUP BY stage
 ORDER BY 2 DESC;
--- insight
+-- insight;
 -- Late-stage companies experienced the highest layoffs peaking at post-IPO with 204,073 layoffs.
 
 -- COMPANY WITH THE HIGHEST TOTAL LAID OFF
@@ -289,7 +291,7 @@ SELECT company, SUM(total_laid_off)
 FROM layoffs_staging4
 GROUP BY company
 ORDER BY 2 DESC;
--- insight
+-- insight;
 -- Amazon has the highest total layoffs of 18,150
 
 SELECT SUBSTRING(`date`,1,7) AS `month`, SUM(total_laid_off)
@@ -312,7 +314,7 @@ ORDER BY 1 ASC
 SELECT 	`month`, total_off,
 SUM(total_off) OVER(ORDER BY `month`) AS rolling_total
 FROM rolling_total;
--- insight
+-- insight;
 -- total number of global layoffs between march 2020 and march 2023 = 382,820
 
 SELECT company, SUM(total_laid_off)
